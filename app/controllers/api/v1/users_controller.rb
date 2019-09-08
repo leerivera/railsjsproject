31941cb8e7acdf86bb91e287a4e3993944570a0a
github username <github email address>
2019-09-08 18:22:50 -0400

    users controller fininshed

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,34 @@
+class Api::V1::UsersController < ApplicationController
+	before_action :find_user, only: [:update]
+	def index
+		@users = User.all
+		render json: @users
+	end
+	def create
+    
+		@user = User.create(user_params)
+
+		render json: @user, status: 200
+	end
+
+   def show
+    @user = User.find(params[:id])
+
+    render json: @user, status: 200  
+   end
+
+	def update
+		@user = User.find(params[:id])
+
+		render json: @product, status: 200 
+	end
+	private
+
+	 def user_params
+      params.permit(:name, :email)
+     end
+
+     def find_user
+       @user = User.find(params[:id])
+     end
+end
